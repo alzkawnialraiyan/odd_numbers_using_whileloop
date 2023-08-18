8ed53d6bf6ac3975eafdb7b8f9c673ed3dc9de44
al raiyan al zakwani <alzakwanialraiyan0@gmail.com>
2023-08-18 13:08:16 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //odd numbers using while loop
+        int odd = 1;
+        while(odd<=100){
+            if(odd%2!=0);
+            System.out.println(odd);
+            odd+=2;
+        }
+    }
+}
